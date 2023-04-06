99af042d1f36170941b0e05184429a4023a4ca0a
erblack <erblack@practicum.com>
2023-04-06 09:47:13 +0200

    Simplify copy

--- bin/init.js
+++ bin/init.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const { Command } = require('commander');
-const TaskInit = require('../lib/TaskInit');
+const copy = require('../lib/copy');
 const { join, resolve } = require('path');
 
 const program = new Command();
@@ -11,9 +11,7 @@ program
     .option('-o, --override [override]', 'Перезаписать файлы и папки дефолтных настроек', Boolean)
     .option('-d, --dry-run [dryRun]', 'Не создавать файлы и папки', Boolean)
     .action(({ override, dryRun }) => {
-        const taskInit = new TaskInit();
-
-        taskInit.copy(join(resolve(__dirname, '..'), 'bemg'), join(resolve(process.cwd(), './bemg')), override, dryRun);
+        copy(join(resolve(__dirname, '..'), 'bemg'), join(resolve(process.cwd(), './bemg')), override, dryRun);
     });
 
 program.parse(process.argv);
